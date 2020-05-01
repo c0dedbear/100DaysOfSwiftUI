12cb466907562ebc4b6b85efe10900cfd6bb0bf1
Mikhail Medvedev <mi.medvedev@cft.ru>
2020-05-01 14:56:54 +0300

    Day 50 completed

---
+++ Project10-CupcakeCorner/Project10-CupcakeCorner/Views/CheckoutView.swift
@@ -0,0 +1,41 @@
+//
+//  CheckoutView.swift
+//  Project10-CupcakeCorner
+//
+//  Created by Михаил Медведев on 01.05.2020.
+//  Copyright © 2020 Михаил Медведев. All rights reserved.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+	@ObservedObject var order: Order
+
+	var body: some View {
+		GeometryReader { geo in
+			ScrollView {
+				VStack {
+					Image("cupcakes")
+						.resizable()
+						.scaledToFit()
+						.frame(width: geo.size.width)
+
+					Text("Your total is $\(self.order.cost, specifier: "%.2f")")
+						.font(.title)
+
+					Button("Place Order") {
+						// place the order
+					}
+					.padding()
+				}
+			}
+		}
+		.navigationBarTitle("Check out", displayMode: .inline)
+	}
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+	static var previews: some View {
+		CheckoutView(order: Order())
+	}
+}
