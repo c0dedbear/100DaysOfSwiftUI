bedfb0503594cb512a712a55029b2e5ffa5aacd6
Mikhail Medvedev <mi.medvedev@cft.ru>
2020-05-06 09:09:57 +0300

    Day 54 completed

--- Project11-Bookworm/Project11-Bookworm/ContentView.swift
+++ Project11-Bookworm/Project11-Bookworm/ContentView.swift
@@ -9,13 +9,37 @@
 import SwiftUI
 
 struct ContentView: View {
+	@Environment(\.managedObjectContext) var moc
+	@FetchRequest(entity: Book.entity(), sortDescriptors: []) var books: FetchedResults<Book>
+
+	@State private var showingAddScreen = false
 
 	var body: some View {
-		VStack() {
-			Text("Creating a custom component with @Binding")
-			CustomComponentwithBindingView().padding()
-			Text("Combine Core Data and SwiftUI")
-			CoreDataView()
+		NavigationView {
+			List {
+				ForEach(books, id: \.self) { book in
+					NavigationLink(destination: Text(book.title ?? "Unknown Title")) {
+						EmojiRatingView(rating: book.rating)
+							.font(.largeTitle)
+
+						VStack(alignment: .leading) {
+							Text(book.title ?? "Unknown Title")
+								.font(.headline)
+							Text(book.author ?? "Unknown Author")
+								.foregroundColor(.secondary)
+						}
+					}
+				}
+			}
+				.navigationBarTitle("Bookworm")
+				.navigationBarItems(trailing: Button(action: {
+					self.showingAddScreen.toggle()
+				}) {
+					Image(systemName: "plus")
+				})
+				.sheet(isPresented: $showingAddScreen) {
+					AddBookView().environment(\.managedObjectContext, self.moc)
+			}
 		}
 	}
 }
