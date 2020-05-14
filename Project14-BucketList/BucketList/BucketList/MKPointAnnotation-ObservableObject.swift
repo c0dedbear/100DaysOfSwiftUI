e76ae360b7807ed87b288556d4c1f9d77bcb6611
c0dedbear <icodedbear@gmail.com>
2020-05-14 18:45:51 +0300

    Day 71 completed (Project14-BucketList)

---
+++ Project14-BucketList/BucketList/BucketList/MKPointAnnotation-ObservableObject.swift
@@ -0,0 +1,31 @@
+//
+//  MKPointAnnotation-ObservableObject.swift
+//  BucketList
+//
+//  Created by Mikhail Medvedev on 14.05.2020.
+//  Copyright © 2020 Mikhail Medvedev. All rights reserved.
+//
+
+import MapKit
+
+extension MKPointAnnotation: ObservableObject {
+    public var wrappedTitle: String {
+        get {
+            self.title ?? "Unknown value"
+        }
+
+        set {
+            title = newValue
+        }
+    }
+
+    public var wrappedSubtitle: String {
+        get {
+            self.subtitle ?? "Unknown value"
+        }
+
+        set {
+            subtitle = newValue
+        }
+    }
+}
