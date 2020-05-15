b6e1b400c65a3b68df5a7b87e319827621bfef0b
Mikhail Medvedev <mi.medvedev@cft.ru>
2020-05-15 18:03:42 +0300

    Day 74 completed(Project15-Accessibility)

---
+++ Project15-Accessibility/Accessibility/Accessibility/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Accessibility
+//
+//  Created by Михаил Медведев on 15.05.2020.
+//  Copyright © 2020 Михаил Медведев. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
