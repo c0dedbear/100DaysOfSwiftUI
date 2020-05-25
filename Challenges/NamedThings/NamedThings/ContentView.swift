5bbe43e0b06337900e46f16f02263381b3692c65
c0dedbear <icodedbear@gmail.com>
2020-05-25 09:04:44 +0300

    Day 77 started (NamedThings challenge)

---
+++ Challenges/NamedThings/NamedThings/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NamedThings
+//
+//  Created by Mikhail Medvedev on 16.05.2020.
+//  Copyright © 2020 Mikhail Medvedev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        PhotoCardView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
