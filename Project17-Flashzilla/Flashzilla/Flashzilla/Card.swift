7d8bfa0249d34146f8b6fd91b079ef926f2f7da3
Mikhail Medvedev <mi.medvedev@cft.ru>
2020-06-02 20:11:56 +0300

    Days 86-88 completed (Project17-Flashzilla)

---
+++ Project17-Flashzilla/Flashzilla/Flashzilla/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Михаил Медведев on 02.06.2020.
+//  Copyright © 2020 Михаил Медведев. All rights reserved.
+//
+
+struct Card {
+    let prompt: String
+    let answer: String
+
+    static var example: Card {
+        Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+    }
+}
